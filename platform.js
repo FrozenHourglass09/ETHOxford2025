97c74a6ca58c9bc5e8359dca44598a55409c434c
AurelienRousche <aurelien.rousche@std.heh.be>
2025-02-08 17:58:05 +0100

    add: platforms

---
+++ platform.js
@@ -0,0 +1,10 @@
+class Platform {
+    constructor(pos_x, pos_y, height, width){
+        image = this.load.image('platform', 'assets/Background/ground.jpg');
+        pos_x = pos_x;
+        pos_y = pos_y;
+        height = height;
+        width = width;
+        this.sprite = this.add.tileSprite(pos_x,pos_y,width,height,'platform').setOrigin(0,0);
+    }
+}
